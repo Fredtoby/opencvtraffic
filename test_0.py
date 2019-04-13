5a05ca5c7633f9c8a52f9dc5c2c5b399e393db67
zhenningli <fredtoby@yeah.net>
2019-04-12 16:11:06 -1000

    Add files via upload

---
+++ test_0.py
@@ -0,0 +1,169 @@
+from test import *
+import imutils
+import time
+import cv2
+import os
+import glob
+import numpy 
+
+tracker = Sort()
+memory = {}
+line = [(0, 1000), (2560, 1000)]
+counter = 1
+counter_up = 1
+counter_down = 0
+
+def intersect(A, B, C, D):
+    return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
+
+
+def ccw(A, B, C):
+    return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
+
+
+labelsPath = "./yolo-coco/coco.names"
+LABELS = open(labelsPath).read().strip().split("\n")
+
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(200, 3),dtype="uint8")
+
+weightsPath = "./yolo-coco/yolov3.weights"
+configPath = "./yolo-coco/yolov3.cfg"
+
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+#vs = cv2.VideoCapture("./input/test.mp4")
+vs = cv2.VideoCapture("./input/aaa.avi")
+writer = None
+(W, H) = (None, None)
+
+frameIndex = 0
+
+try:
+    prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+        else cv2.CAP_PROP_FRAME_COUNT
+    total = int(vs.get(prop))
+    print("[INFO] {} total frames in video".format(total))
+
+except:
+    print("[INFO] could not determine # of frames in video")
+    print("[INFO] no approx. completion time can be provided")
+    total = -1
+
+while True:
+    (grabbed, frame) = vs.read()
+
+    if not grabbed:
+        break
+
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            if confidence > 0.3:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
+
+    dets = []
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            dets.append([x, y, x + w, y + h, confidences[i]])
+
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+    dets = np.asarray(dets)
+    tracks = tracker.update(dets)
+
+    boxes = []
+    indexIDs = []
+    c = []
+    previous = memory.copy()
+    memory = {}
+
+    for track in tracks:
+        boxes.append([track[0], track[1], track[2], track[3]])
+        indexIDs.append(int(track[4]))
+        memory[indexIDs[-1]] = boxes[-1]
+
+    if len(boxes) > 0:
+        i = int(0)
+        for box in boxes:
+            (x, y) = (int(box[0]), int(box[1]))
+            (w, h) = (int(box[2]), int(box[3]))
+
+            color = [int(c) for c in COLORS[indexIDs[i] % len(COLORS)]]
+            cv2.rectangle(frame, (x, y), (w, h), color, 2)
+
+            if indexIDs[i] in previous:
+                previous_box = previous[indexIDs[i]]
+                (x2, y2) = (int(previous_box[0]), int(previous_box[1]))
+                (w2, h2) = (int(previous_box[2]), int(previous_box[3]))
+                p0 = (int(x + (w - x) / 2), int(y + (h - y) / 2))
+                p1 = (int(x2 + (w2 - x2) / 2), int(y2 + (h2 - y2) / 2))
+                cv2.line(frame, p0, p1, color, 3)
+
+                if intersect(p0, p1, line[0], line[1]):
+                    counter += 1
+
+                    if y2 < y:
+                        counter_down += 1
+                    else:
+                        counter_up += 1
+
+            i += 1
+
+    cv2.line(frame, line[0], line[1], (0, 255, 0), 5)
+    cv2.putText(frame, str(counter), (100, 200), cv2.FONT_HERSHEY_DUPLEX, 5.0, (0, 0, 255), 10)
+    cv2.putText(frame, str(counter_up), (500, 200), cv2.FONT_HERSHEY_DUPLEX, 5.0, (0, 255, 0), 10)
+    cv2.putText(frame, str(counter_down), (900, 200), cv2.FONT_HERSHEY_DUPLEX, 5.0, (255, 0, 0), 10)
+
+
+    if writer is None:
+        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+        writer = cv2.VideoWriter("output/test2.mp4", fourcc, 30,
+                                 (frame.shape[1], frame.shape[0]), True)
+
+        if total > 0:
+            elap = (end - start)
+            print("[INFO] single frame took {:.4f} seconds".format(elap))
+            print("[INFO] estimated total time to finish: {:.4f}".format(
+                elap * total))
+
+    writer.write(frame)
+    # cv2.imshow("", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    cv2.destroyAllWindows()
+
+    frameIndex += 1
+
+
+writer.release()
+vs.release()
